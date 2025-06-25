8e1f2331e9c87529c8a2ea894cf612d152f429da
Micho F. Reyes <michoreyes15@gmail.com>
2025-06-25 12:56:40 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/Header/Header";
 import DesktopHero from "@/components/Hero/DesktopHero";
-import Image from "next/image";
 
 export default function Home() {
   return (
